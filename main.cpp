5e29bea330644c1d1447f762a2b140481d776c46
Zac Johnston <24073zj@hvhs.school.nz>
2026-06-25 06:47:59 +1200

    Create WASD input

--- main.cpp
+++ main.cpp
@@ -2,49 +2,110 @@
 #include <TGUI/TGUI.hpp>
 #include <TGUI/Backend/SFML-Graphics.hpp>
 #include <iostream>
-#include <optional>
 
+const int screenw = 800;
+const int screenh = 600;
+
+bool menu = true;
+
+
+float playerx = 0;
+float playery = 0;
+
+sf::RectangleShape player;
+
+bool upkey = false;
+bool downkey = false;
+bool leftkey = false;
+bool rightkey = false;
+
+void Reset()
+{
+
+}
+
+void Logic() {
+    if (menu) {
+        
+    }
+    else {
+        if (upkey)
+        {
+			playery -= 5;
+        }
+        if (downkey)
+        {
+            playery += 5;
+        }
+        if (leftkey)
+        {
+            playerx -= 5;
+        }
+        if (rightkey)
+        {
+            playerx += 5;
+        }
+
+		player.setPosition(sf::Vector2f(playerx, playery));
+    }
+}
 int main() {
-    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Platformer");
+    sf::RenderWindow window(sf::VideoMode({ screenw, screenh }), "Platformer");
     window.setFramerateLimit(60);
+    // init TGUI
+    tgui::Gui menuGui(window);
+    tgui::Gui playGui(window);
 
-    // init tgui
-    tgui::Gui gui(window);
+    // SFML objects
+    // player
+    player.setFillColor(sf::Color(150, 0, 0));
+    player.setSize(sf::Vector2f(50, 50));
+    player.setPosition(sf::Vector2f( playerx, playery ));
 
-    // tgui stuff
-
+    // TGUI stuff
     // title
     auto titleLabel = tgui::Label::create();
-    titleLabel->setText("Label text");
-    titleLabel->setTextSize(24);
-    titleLabel->setPosition(220, 40);
+    titleLabel->setText("C++ Platformer");
+    titleLabel->setTextSize(30);
+    titleLabel->setPosition(280, 50);
     titleLabel->getRenderer()->setTextColor(tgui::Color::White);
-    gui.add(titleLabel);
-
-    // text box
-    auto editBox = tgui::EditBox::create();
-    editBox->setSize(200, 40);
-    editBox->setPosition(300, 320);
-    editBox->setDefaultText("type something");
-    gui.add(editBox);
+    menuGui.add(titleLabel);
 
     // button
-    auto actionButton = tgui::Button::create("Apply Color");
+    auto actionButton = tgui::Button::create("Play");
     actionButton->setSize(120, 40);
-    actionButton->setPosition(340, 380);
-    gui.add(actionButton);
+    actionButton->setPosition(340, 100);
+    menuGui.add(actionButton);
 
     // add actions to elements
     actionButton->onPress([&]() {
-        // get text input
-        std::string text = editBox->getText().toStdString();
-        });
+		menu = false;
+    });
 
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
-
             // update ui events
-            gui.handleEvent(*event);
+            menuGui.handleEvent(*event);
+            
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+                upkey = true;
+            else 
+                upkey = false;
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+                leftkey = true;
+            else
+                leftkey = false;
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+                downkey = true;
+            else 
+                downkey = false;
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+                rightkey = true;
+            else 
+                rightkey = false;
 
             // close window
             if (event->is<sf::Event::Closed>() || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)) {
@@ -52,15 +113,33 @@ int main() {
             }
         }
 
+        Logic();
+
         // drawing
-        window.clear(sf::Color(255, 255, 255));
+        window.clear(sf::Color(0, 0, 0));
 
         // draw sfml before tgui
+        if (menu)
+        {
+            
+        }
+        else
+        {
+            window.draw(player);
+        }
 
         // draw tgui
-        gui.draw();
+        if (menu)
+        {
+            menuGui.draw();
+        }
+        else
+        {
+            playGui.draw();
+        }
 
         window.display();
+        std::cout << "W: " << upkey << " A: " << leftkey << " S: " << downkey << " D:" << rightkey << std::endl;
     }
 
     return 0;
